5df362d30845411d5f7de95e4e8afec215712138
Kerry D. Wong <kerryw@uwalumni.com>
2012-10-01 19:14:49 -0400

    updated documentation

--- doc/html/mcp2210test_8cpp.js
+++ doc/html/mcp2210test_8cpp.js
@@ -2,6 +2,6 @@ var mcp2210test_8cpp =
 [
     [ "main", "mcp2210test_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627", null ],
     [ "TestGPIO", "mcp2210test_8cpp.html#a83e0584577191d7a713a51caf092e15a", null ],
-    [ "TestMCP23s08", "mcp2210test_8cpp.html#a46af0f2b99142b24c0037e292ae1be08", null ],
+    [ "TestMCP23S08", "mcp2210test_8cpp.html#ab2e9361f5f16dcd7d0858e8276042338", null ],
     [ "TestMisc", "mcp2210test_8cpp.html#a2103aa7c9eefc74cf38aa93cbaae6a97", null ]
 ];
--- doc/html/search/all_74.js
+++ doc/html/search/all_74.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['testgpio',['TestGPIO',['../mcp2210test_8cpp.html#a83e0584577191d7a713a51caf092e15a',1,'mcp2210test.cpp']]],
-  ['testmcp23s08',['TestMCP23s08',['../mcp2210test_8cpp.html#a46af0f2b99142b24c0037e292ae1be08',1,'mcp2210test.cpp']]],
+  ['testmcp23s08',['TestMCP23S08',['../mcp2210test_8cpp.html#ab2e9361f5f16dcd7d0858e8276042338',1,'mcp2210test.cpp']]],
   ['testmisc',['TestMisc',['../mcp2210test_8cpp.html#a2103aa7c9eefc74cf38aa93cbaae6a97',1,'mcp2210test.cpp']]]
 ];
