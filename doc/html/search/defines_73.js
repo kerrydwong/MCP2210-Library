fdf32d9ae972d078e39785fd2dc5425c04359587
Kerry D. Wong <kerryw@uwalumni.com>
2012-10-27 21:36:43 -0400

    updated documentation

---
+++ doc/html/search/defines_73.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['spi_5fstatus_5ffinished_5fno_5fdata_5fto_5fsend',['SPI_STATUS_FINISHED_NO_DATA_TO_SEND',['../mcp2210_8h.html#a239de9f10112b514560b093978f95734',1,'mcp2210.h']]],
+  ['spi_5fstatus_5fstarted_5fno_5fdata_5fto_5freceive',['SPI_STATUS_STARTED_NO_DATA_TO_RECEIVE',['../mcp2210_8h.html#af8fa0a51c85ba6f7d77f50e363a4b8de',1,'mcp2210.h']]],
+  ['spi_5fstatus_5fsuccessful',['SPI_STATUS_SUCCESSFUL',['../mcp2210_8h.html#adf038bac4ab1195954a3c12578c2056e',1,'mcp2210.h']]]
+];
